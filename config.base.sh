7f9bb135bb368b29bcf94f15ef8fad975a7fd376
Jami Kettunen <jami.kettunen@protonmail.com>
2021-11-05 23:27:39 +0200

    config.base.sh: Install strace & tree by default as well

--- config.base.sh
+++ config.base.sh
@@ -69,7 +69,7 @@ base_pkgs=(
 
 	zsh zsh-completions zsh-autosuggestions # Shell
 	#zsh-history-substring-search zsh-syntax-highlighting
-	git htop neovim neofetch psmisc wget curl conspy xtools xxd ripgrep # Some tools
+	git htop neovim neofetch psmisc wget curl conspy xtools xxd ripgrep strace tree # Some tools
 )
 
 extra_build_pkgs=(
