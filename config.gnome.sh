beded2aa9504b35b740434ff3f5a352562eccc0a
Jami Kettunen <jami.kettunen@protonmail.com>
2021-11-07 21:17:03 +0200

    config.gnome.sh: Install Waydroid + mutter-bgr

--- config.gnome.sh
+++ config.gnome.sh
@@ -7,11 +7,13 @@ extra_build_pkgs+=(
 	mesa
 	portfolio pmos-tweaks mobile-config-firefox
 	megapixels callaudiod sofia-sip gnome-calls libphonenumber purism-chatty
+	libglibutil libgbinder python3-gbinder waydroid mutter-bgr
 )
 extra_install_pkgs+=(
 	mesa-vulkan-freedreno mesa-vulkan-overlay-layer mesa-tools
 	portfolio pmos-tweaks mobile-config-firefox
 	megapixels gnome-calls purism-chatty
+	waydroid mutter-bgr
 )
 unset overlays[-1] # drop somainline overlay to apply it after others
 overlays+=(ui-gnome pipewire-audio somainline)
