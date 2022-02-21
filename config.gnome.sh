220ab8a76144880e90f18a4b1371b43e2ab1b43e
Jami Kettunen <jami.kettunen@protonmail.com>
2022-02-21 12:38:36 +0200

    config.gnome.sh: Use mobile-config-firefox from repos

--- config.gnome.sh
+++ config.gnome.sh
@@ -1,16 +1,16 @@
 . config.base.sh
 
 base_pkgs+=(
-	firefox # 192M
+	firefox mobile-config-firefox # 192M
 )
 extra_build_pkgs+=(
 	mesa
-	portfolio pmos-tweaks mobile-config-firefox
+	portfolio pmos-tweaks
 	megapixels callaudiod sofia-sip gnome-calls purism-chatty
 )
 extra_install_pkgs+=(
 	mesa-vulkan-freedreno mesa-vulkan-overlay-layer mesa-tools
-	portfolio pmos-tweaks mobile-config-firefox
+	portfolio pmos-tweaks
 	megapixels gnome-calls purism-chatty
 )
 unset overlays[-1] # drop somainline overlay to apply it after others
