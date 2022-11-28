be0399f7583facd14f9286e149afdd98120f6e02
Jami Kettunen <jami.kettunen@protonmail.com>
2022-11-28 03:33:00 +0200

    config.gnome.sh: Tidy dropping somainline overlay + updated comments

--- config.gnome.sh
+++ config.gnome.sh
@@ -1,7 +1,7 @@
 . config.base.sh
 
 base_pkgs+=(
-	Vulkan-Tools # vkcube, ...
+	Vulkan-Tools # vkcube, ... (after merge of https://github.com/void-linux/void-packages/pull/40747)
 	firefox mobile-config-firefox # 192M
 	dejavu-fonts-ttf # for proper font rendering in ff :)
 	noto-fonts-emoji # for emoji keyboard on gnome shell mobile
@@ -17,13 +17,19 @@ extra_install_pkgs+=(
 	portfolio pmos-tweaks
 	megapixels gnome-calls purism-chatty
 )
-unset overlays[-1] # drop somainline overlay to apply it after others
-overlays+=(
+overlays=(
+	# drop somainline overlay temporarily to apply it after EVERYTHING else
+	"${overlays[@]/somainline}"
+
 	ui-gnome
 	firefox-wayland
 	pipewire-audio
 	qcom_spmi_haptics-feedbackd
+
 	somainline
 )
-enable_sv=("${enable_sv[@]/buffyboard}") # drop buffyboard sv to avoid input issues in GNOME
+enable_sv=(
+	# drop buffyboard sv to avoid potential input issues in GNOME (https://gitlab.com/cherrypicker/buffyboard/-/issues/21)
+	"${enable_sv[@]/buffyboard}"
+)
 img_name_format="${img_name_format/-base/-gnome}"
