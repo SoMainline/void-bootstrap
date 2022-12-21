4a4ac54de71de2956d8a9fb137a93fe32894d3c8
Jami Kettunen <jami.kettunen@protonmail.com>
2022-12-22 01:42:25 +0200

    overlay: neard-tests: Don't explicitly install python(2)
    It's already pulled in by python-dbus & python-gobject.

--- overlay/neard-tests/deploy.sh
+++ overlay/neard-tests/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-xbps-install -y wget neard python{,-dbus,-gobject}
+xbps-install -y wget neard python-{dbus,gobject}
 wget https://github.com/linux-nfc/neard/archive/master.tar.gz -t 3 --show-progress -qO /tmp/neard.tar.gz
 mkdir -p /usr/libexec/nfc/test
 tar -C /usr/libexec/nfc/test -xf /tmp/neard.tar.gz neard-master/test --strip-components=2
